d4493ed9253dabe8b240af34db1d1b86315018ae
Christos <xristos94sideris@gmail.com>
2020-02-09 15:05:11 +0200

    Fixed order search by date error

--- src/main/java/com/schoolcanteen/app/repository/OrderRepository.java
+++ src/main/java/com/schoolcanteen/app/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
@@ -12,17 +13,17 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findByRegn(String regn);
 
-    List<Order> findByDateAfter(LocalDate dateAfter);
-
     List<Order> findByDateBefore(LocalDate dateBefore);
 
-    List<Order> findByDateBetween(LocalDate dateAfter, LocalDate dateBefore);
+    List<Object> findByDateAfter(LocalDate dateAfter);
+
+    List<Order> findByDateBetween(LocalDate dateUntil, LocalDate dateBefore);
 
     List<Order> findByRegnAndDateBefore(String regn, LocalDate dateBefore);
 
     List<Order> findByRegnAndDateAfter(String regn, LocalDate dateAfter);
 
-    List<Order> findByRegnAndDateBetween(String regn, LocalDate dateAfter, LocalDate dateBefore);
+    List<Order> findByRegnAndDateBetween(String regn, LocalDate dateUntil, LocalDate dateBefore);
 
 
     Order save(Order order);
